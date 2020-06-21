b491c6da497d0913cd16cadac5378983bd2266f0
Diego Teixeira dos Santos <ogeidexe@gmail.com>
2020-06-21 02:01:05 +0000

    RF03

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -8,21 +8,23 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class CadastroComponent implements OnInit {
   cadastroForm:FormGroup;
-  checked:boolean;
+  
   constructor() { }
-
   ngOnInit(): void {
     if(true){
       this.cadastroForm =  new FormGroup({
-        cnpj: new FormControl('works'),
+        cnpj: new FormControl(''),
         logradouro: new FormControl(''),
         numero: new FormControl(''),
         bairro: new FormControl(''),
         cep: new FormControl(''),
         cidade: new FormControl(''),
         estado: new FormControl(''),
-        Observação : new FormControl(''),
-      })
+        observacao : new FormControl(''),
+        isChecked:new FormControl(false)
+      });
+      this.cadastroForm.get('isChecked');
+
     }else{
       this.cadastroForm =  new FormGroup({
         cnpj: new FormControl(''),
